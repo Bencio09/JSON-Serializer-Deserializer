827809ef99be79109a54ddbebcbdc1bc183c90a9
lai2-01 <lai2-01@itismeucci.com>
2022-01-31 11:50:53 +0100

    commit

---
+++ src/main/java/com/bencini/itismeucci/aula.java
@@ -0,0 +1,15 @@
+package com.bencini.itismeucci;
+
+public class aula {
+
+    String nome;
+    String numeroDiBanchi;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getNumeroDiBanchi() {
+        return numeroDiBanchi;
+    }
+}
---
+++ src/main/java/com/bencini/itismeucci/element.java
@@ -0,0 +1,20 @@
+package com.bencini.itismeucci;
+
+public class element {
+    String annoDiNascita;
+    String cognome;
+    String nome;
+
+    public String getAnnoDiNascita() {
+        return annoDiNascita;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+}
